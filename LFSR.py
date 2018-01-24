c4a3337249bdfcac8f237a9d9035f09a28fb1b2d
Blair Sharpe <blair.a.sharpe@gmail.com>
2018-01-24 12:26:31 -0500

    Added XNOR capabilities

--- LFSR.py
+++ LFSR.py
@@ -1,4 +1,4 @@
-def xor(state, inputs, length):
+def xor(state, inputs, length, invert):
     """Computes XOR digital logic
 
     Parameters:
@@ -15,23 +15,33 @@ def xor(state, inputs, length):
     input_a = int(state[inputs[0]])
     input_b = int(state[inputs[1]])
 
-    result = (input_a & ~input_b) | (~input_a & input_b)
+    result = bool((input_a & ~input_b) | (~input_a & input_b))
 
+    # Checks if an XNOR is needed
+    if invert is True: result = not result
+
+    # Shift result of xor to MSB
     return result << length
 
 
 if __name__ == "__main__":
 
-    current_state = "001"
+    current_state = "00000000000000000000000000000001"
+
+    # Position to tap for Xor gate
     index_inputs = [0, 2]
-    max_clock = 7
+    max_clock = 100
+    invert = True
 
     for clock in range(0, max_clock + 1):
 
         print(clock, current_state)
-        xor_output = xor(current_state, index_inputs, len(current_state)-1)
+
+        xor_output = xor(current_state, index_inputs, len(current_state) - 1,
+                         invert)
+
         shift = int(current_state, 2) >> 1
+
+        # Re-assign the current state and pad with zeroes
         current_state = format(xor_output | shift, '0{}b'.format(len(
             current_state)))
-
-
